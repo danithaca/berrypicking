a138e8c3285699f8d23c996205bc5a84250aae6f
Daniel Zhou (TechBrewery) <danithaca@gmail.com>
2014-12-10 16:10:39 -0500

    change django stuff

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ berrypicking
 ============
 
 My code playground and reusable code libraries.
+
+Directories
+-----------
+
+  * django: code examples for django
+    - mysite-1.6: based on django tutorial 1.6. no more updates.
+    - mysite-1.7: based on django tutorial 1.7. migrated from mysite-1.6. keeps updating.
--- django/mysite-1.7/mysite/views.py
+++ django/mysite-1.7/mysite/views.py
@@ -2,8 +2,12 @@ from django.contrib.auth.models import User
 from django.contrib.auth.views import login
 from django.http import HttpResponse
 import json
+from django.template.response import TemplateResponse
 
 
 def home(request):
-    return HttpResponse("hello, %s" % str(request.user))
+    return TemplateResponse(request, 'mysite/index.html', context={
+        'message': 'hello foobar'
+    })
+    #return HttpResponse("hello, %s" % str(request.user))
     #return login(request)
