576dfe20bc96d906bc2948de17e2056708e17f1d
Daniel Zhou <danithaca@gmail.com>
2014-12-14 10:23:54 -0500

    minor update

--- django/mysite-1.7/mysite/urls.py
+++ django/mysite-1.7/mysite/urls.py
@@ -7,7 +7,9 @@ urlpatterns = patterns('',
     # Examples:
     #url(r'^$', 'django.template.response.TemplateResponse', {'template': 'mysite/home.jinja2', 'context': {'message': 'hello foobar'}}, name='home'),
     url(r'^$', 'mysite.views.home', name='home'),
-    url(r'^login/$', 'django.contrib.auth.views.login', {'extra_context': {'next': '/'}, 'template_name': 'mysite/login.jinja2'}, name='login'),
+    #url(r'^login/$', 'django.contrib.auth.views.login', {'extra_context': {'next': '/'}, 'template_name': 'mysite/login.jinja2'}, name='login'),
+    # no need to set 'next' if LOGIN_REDIRECT_URL is set to /
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'mysite/login.jinja2'}, name='login'),
     #url(r'^user/', include('django.contrib.auth.urls', namespace="user")),
 
     # url(r'^blog/', include('blog.urls')),
