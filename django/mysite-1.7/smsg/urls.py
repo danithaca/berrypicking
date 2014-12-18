4662966b72efad862b5f554bf0447cf4879e0b06
Daniel Zhou (TechBrewery) <danithaca@gmail.com>
2014-12-18 14:15:44 -0500

    update

--- django/mysite-1.7/smsg/urls.py
+++ django/mysite-1.7/smsg/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-from smsg.views import ListView, add
+from smsg.views import ListView, add, EditView
 
 urlpatterns = patterns('',
    url(r'^$', ListView.as_view(), name='list'),
    url(r'^add/$', add, name='add'),
+   url(r'^(?P<pk>\d+)/edit/$', EditView.as_view(), name='edit'),
 )
--- django/mysite-1.7/smsg/views.py
+++ django/mysite-1.7/smsg/views.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, render, redirect
 from django.views import generic
@@ -9,15 +10,21 @@ class ListView(generic.ListView):
     template_name = 'smsg/list.jinja2'
 
 
+class EditView(generic.UpdateView):
+    model = Message
+    template_name = 'smsg/edit.jinja2'
+    success_url = reverse_lazy('smsg:list')
+
+
 def add(request):
     #return HttpResponse('ok.')
     if request.method == 'POST':
         form = MessageForm(request.POST)
         if form.is_valid():
-           form.save()
-        return redirect('smsg:list')
+            form.save()
+            return redirect('smsg:list')
         #return HttpResponseRedirect('/smsg')
     else:
         form = MessageForm()
         # this has to have "request" as input, otherwise csrf_token would not be included.
-        return render(request, 'smsg/add.jinja2', {'form': form})
+    return render(request, 'smsg/add.jinja2', {'form': form})
