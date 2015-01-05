8669728321410c9efd725fdac73c54c3873be179
Daniel Zhou (TechBrewery) <danithaca@gmail.com>
2015-01-05 15:49:29 -0500

    small update and angular phonecat

--- django/mysite-1.7/mysite/views.py
+++ django/mysite-1.7/mysite/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.views import login
+from django.core.mail import send_mail
 from django.http import HttpResponse
 import json
 from django.template.response import TemplateResponse
@@ -13,3 +14,7 @@ def home(request):
     })
     #return HttpResponse("hello, %s" % str(request.user))
     #return login(request)
+
+
+def demo(request):
+    send_mail('Greetings %s' % request.user.get_username(), 'test message', from_email='danithaca@gmail.com', recipient_list=('mrzhou@umich.edu',))
