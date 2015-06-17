85fe5286b8f200e2f1bdfbd66c3f0aa78666ee41
Daniel Zhou (TechBrewery) <danithaca@gmail.com>
2015-06-17 10:01:30 -0400

    celery trial

--- celery/tasks.py
+++ celery/tasks.py
@@ -1,10 +1,19 @@
 from celery import Celery
 
+# to run:
+# celery -A tasks worker --loglevel=info
+
+# the `-A` finds the tasks module under PYTHON_PATH, which is in current working folder
+
+
 # sqlite (filename)
 BROKER_URL = 'sqla+sqlite:///celerydb.sqlite'
+CELERY_RESULT_BACKEND = 'db+sqlite:///celerydb.sqlite'
 
-# app = Celery('tasks', broker='sqla+sqlite:///celerydb.sqlite')
-app = Celery('tasks')
+# first arg is the module to register with celery. might be other module names other than 'tasks'
+# but we are using the same module for convenience.
+app = Celery('tasks', broker=BROKER_URL, backend=CELERY_RESULT_BACKEND)
+# app = Celery('tasks')
 
 @app.task
 def add(x, y):
