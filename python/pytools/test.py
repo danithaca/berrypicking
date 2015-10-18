e6839c41b2e1286d4e12677beaa80e3ac8a75329
Daniel Zhou (Workstation) <danithaca@gmail.com>
2015-10-18 17:00:51 -0400

    updated select2

--- python/pytools/test.py
+++ python/pytools/test.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
+import warnings
 from pytools.algorithms import find_match_tokens
+from pytools.utils import deprecated, new_deprecated
 
 
 class TestAlgorithm(TestCase):
@@ -7,3 +9,18 @@ class TestAlgorithm(TestCase):
         self.assertEqual(['abcdef'], find_match_tokens('abcdef .', ' abcdef'))
         self.assertEqual(set(['abc', 'def']), set(find_match_tokens('xabc zdef .', ' abcdef')))
         self.assertEqual(set(['abc', 'def']), set(find_match_tokens(' zdef xabc.', ' abcdef')))
+
+
+# class TestUtils(TestCase):
+#     @deprecated
+#     def dep(self):
+#         print('dep')
+#
+#     def testDecorators(self):
+#         self.dep()
+
+@new_deprecated
+def dep():
+    print('dep')
+
+dep()
