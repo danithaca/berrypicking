b48e387e799ad4e0a3280f37b244136c7dcbb2ea
Daniel Zhou (Windows) <danithaca@gmail.com>
2015-12-03 15:22:19 -0500

    added beautiful souop

---
+++ python/example/beautifulsoup.py
@@ -0,0 +1,31 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import re
+import json
+
+page = urllib.request.urlopen('http://www-01.sil.org/linguistics/GlossaryOfLinguisticTerms/contents.htm').read()
+soup = BeautifulSoup(page, 'html.parser')
+prefix = 'http://www-01.sil.org/linguistics/GlossaryOfLinguisticTerms/'
+text_pattern = r'What (?:is|are) (?:a |an |the )?(.+)\?(.*)'
+data = []
+
+for link in soup.find_all('a'):
+    text = link.get_text()
+    href = link.get('href')
+    if text.startswith('What') and href.startswith('What'):
+        match = re.match(text_pattern, text)
+        #print(text)
+        extracted = (match.group(1) + match.group(2)).title()
+        # print(extracted.title())
+        full_url = prefix + href
+        data.append({
+            'term': extracted,
+            'url': full_url
+        })
+        # node = BeautifulSoup(urllib.request.urlopen(full_url).read(), 'html.parser')
+        # print(node.body)
+        #print(href)
+
+with open('crawled.json', 'w') as f:
+    json.dump(data, f)
+
